aa080bf71bfa8cb77104d6ceeb5254d88c2a7745
Valentina Virga <valentina.virga@codestorm.it>
2026-04-27 15:52:09 +0200

    deploy free tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "aws_key_pair" "deploy_key" {
 
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = aws_key_pair.deploy_key.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
