b2bb7262eb90c8f5c8bba7ba97e8106dc528c558
Valentina Virga <valentina.virga@codestorm.it>
2026-04-27 15:42:26 +0200

    deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,11 +30,21 @@ resource "aws_security_group" "web_sg" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["024732134153"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
